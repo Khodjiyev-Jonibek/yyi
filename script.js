e37a52db7fa1411c97e33d211bf435e53b97e49e
Khodjiyev-Jonibek <109537735+Khodjiyev-Jonibek@users.noreply.github.com>
2022-07-18 18:29:10 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+let admin = ''
+let name =  'John'
+admin = name
+
+let balance = 25000
+
+let car = 4480
+let food = 890
+
+let invoice = 5500
+let stock = 4200
+
+let total_balance = 0
+
+let x = car + food
+let y = invoice + stock
+let z = balance + y - x
+
+total_balance = z
+
+console.log(admin,total_balance );
